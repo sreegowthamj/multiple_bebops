f59c88faecdfdae98e139ff41fb34455201d8203
Sree Gowtham Josyula <sreegowthamj@gmail.com>
2018-07-19 08:46:28 -0700

    Adding 'sync' to flush the file changes to memory

--- shortpress_3.sh
+++ shortpress_3.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
 #Set the SSID, Password and IP
-SSID='ssid'	# Change this to your SSID
-PW='password' 		# Change this to the password of your wifi network
-IP=192.168.1.101 	# Change this to the desired Bebop IP	
+SSID='Bebpop176216'	# Change this to your SSID
+PW='Bebpop176216' 		# Change this to the password of your wifi network
+IP=192.168.73.1 	# Change this to the desired Bebop IP	
 
 #Connect to defined Network
 BLDC_Test_Bench -M 2 >/dev/null
 sleep 1 &&
 mount -o remount,rw / &&
 sleep 1 &&
+/bin/sync &&
 wpa_passphrase $SSID $PW > /etc/wpa_supplicant.conf && 
-sleep 1 && 
+sleep 1 &&
+/bin/sync &&
 ifconfig eth0 $IP && 
 sleep 1 &&
 mount -o remount,rw / &&
 sleep 1 &&
 wpa_passphrase $SSID $PW > /etc/wpa_supplicant.conf && 
-sleep 1 && 
+sleep 1 &&
+/bin/sync &&
 ifconfig eth0 $IP &&
 sleep 1 &&
 wpa_supplicant -B -D wext -i eth0 -c /etc/wpa_supplicant.conf &&
