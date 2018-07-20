890d1a553274325cca0aaeb37a089cd50e6820d7
Sree Gowtham Josyula <sreegowthamj@gmail.com>
2018-07-20 12:22:35 -0700

    Updating shortpress to  upon file updates

--- shortpress_3.sh
+++ shortpress_3.sh
@@ -1,31 +1,73 @@
 #!/bin/sh
 #Set the SSID, Password and IP
-SSID='Bebpop176216'	# Change this to your SSID
-PW='Bebpop176216' 		# Change this to the password of your wifi network
+SSID='AndroidAP-sgj'	# Change this to your SSID
+PW='yaot2326' 		# Change this to the password of your wifi network
 IP=192.168.73.1 	# Change this to the desired Bebop IP	
 
+#/data/ftp/internal_000/bin/wpa_passphrase $SSID $PW > /etc/wpa_supplicant.conf && 
 #Connect to defined Network
 BLDC_Test_Bench -M 2 >/dev/null
 sleep 1 &&
-mount -o remount,rw / &&
+/bin/mount -o remount,rw / &&
 sleep 1 &&
 /bin/sync &&
-wpa_passphrase $SSID $PW > /etc/wpa_supplicant.conf && 
 sleep 1 &&
 /bin/sync &&
-ifconfig eth0 $IP && 
+ifconfig   && # eth0 $IP && 
 sleep 1 &&
-mount -o remount,rw / &&
+/bin/mount -o remount,rw / &&
 sleep 1 &&
-wpa_passphrase $SSID $PW > /etc/wpa_supplicant.conf && 
 sleep 1 &&
 /bin/sync &&
-ifconfig eth0 $IP &&
 sleep 1 &&
-wpa_supplicant -B -D wext -i eth0 -c /etc/wpa_supplicant.conf &&
+echo "Releasing lease.." | tee -a /data/ftp/internal_000/bin/log.txt &&
+#udhcpc -v -R -i eth0 | tee -a /data/ftp/internal_000/bin/log.txt &&
+ifconfig eth0 down &&
+ifconfig eth0 up &&
+echo "Releasing lease done" | tee -a /data/ftp/internal_000/bin/log.txt &&
 sleep 1 &&
-BLDC_Test_Bench -M 2 >/dev/null
+/bin/sync &&
+echo "wpa_supplicant request initiated" | tee -a /data/ftp/internal_000/bin/log.txt &&
+/data/ftp/internal_000/bin/wpa_supplicant -B -dd  -D wext -i eth0 -c /etc/wpa_supplicant.conf | tee /data/ftp/internal_000/bin/log.txt && 
+echo "completed wpa_supplicant" | tee -a /data/ftp/internal_000/bin/log.txt &&
+sleep 3 &&
+/bin/sync &&
+BLDC_Test_Bench -M 2 || true &&
+sleep 3 &&
+/bin/sync &&
+BLDC_Test_Bench -M 2 || true &&
 sleep 1 &&
-BLDC_Test_Bench -M 2 >/dev/null
-#wait 5 &&
-#/sbin/udhcpc -i eth0 
+sleep 1 &&
+ifconfig eth0 192.168.43.109 &&
+iwconfig eth0 essid 'AndroidAP-sgj' &&
+/bin/sync &&
+sleep 2 &&
+echo "starting udhcpc" | tee -a /data/ftp/internal_000/bin/log.txt &&
+/bin/sync &&
+/sbin/udhcpc -v  -i eth0 -r 192.168.43.109 -H BebopWhite  | tee -a /data/ftp/internal_000/bin/log.txt &&
+/bin/sync &&
+echo "completed udhcpc" | tee -a /data/ftp/internal_000/bin/log.txt &&
+/bin/sync &&
+/data/ftp/internal_000/bin/iwconfig | tee -a /data/ftp/internal_000/bin/log.txt &&
+/bin/sync &&
+ifconfig -a | tee -a /data/ftp/internal_000/bin/log.txt &&
+/bin/sync &&
+route -n | tee -a /data/ftp/internal_000/bin/log.txt &&
+/bin/sync &&
+BLDC_Test_Bench -M 2 >/dev/null  || true &&
+sleep 3 &&
+echo "Adding default gateway" | tee -a /data/ftp/internal_000/bin/log.txt &&
+route add default gw 192.168.43.1 | tee -a /data/ftp/internal_000/bin/log.txt || true &&
+echo "Adding default gateway completed" | tee -a /data/ftp/internal_000/bin/log.txt &&
+BLDC_Test_Bench -M 2 >/dev/null &&
+sleep 3 &&
+/bin/sync &&
+netstat -rn | tee -a /data/ftp/internal_000/bin/log.txt &&
+echo "Adding netstat op completed" | tee -a /data/ftp/internal_000/bin/log.txt &&
+/bin/sync &&
+sleep 3 &&
+echo "trying ping gateway..." | tee -a /data/ftp/internal_000/bin/log.txt &&
+ping 192.168.43.1 | tee -a /data/ftp/internal_000/bin/log.txt &&
+echo "done ping gateway." | tee -a /data/ftp/internal_000/bin/log.txt &&
+/bin/sync &&
+sleep 3
